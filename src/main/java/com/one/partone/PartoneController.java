ba6563a06482fc0f633a1f04badd84a183167931
anupam-170490 <74314156+anupam-170490@users.noreply.github.com>
2020-11-24 21:34:04 +0530

    commit change

---
+++ src/main/java/com/one/partone/PartoneController.java
@@ -0,0 +1,13 @@
+package com.one.partone;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PartoneController {
+
+	@RequestMapping("/hello")
+    public String hello() {
+        return "Hello World RESTful with Spring Boot Application";
+    } 
+}
